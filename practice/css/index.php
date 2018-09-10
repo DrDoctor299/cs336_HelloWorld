038aee1acd8e809678d3e7f983c0e97ef830f414
Joshua Williams <joshuwilliams@csumb.edu>
2018-09-10 06:39:21 +0000

    Why me

---
+++ practice/css/index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <link href="./sheet1.css" rel="stylesheet">
+        <style>
+            img {
+                width: 400px;
+            }
+            p {
+                margin: 10px;
+            }
+
+        </style>
+    </head>
+    <body>
+        <img style="width: 800px" src="https://pastdaily.com/wp-content/uploads/2015/12/Nat-King-Cole-resize-1024x768.jpg">
+        <p>
+            Lorem ipsum dolor sit amet, eos ne verterem disputationi, tale quaerendum nec et, est diam omittam at. Quando laoreet lobortis id his. Ubique noster temporibus in duo. Tantas ridens nam ei, sit quas exerci tacimates ne, sanctus recteque ei quo.
+
+Dico platonem in quo. Simul nusquam dolores ea sed. Te est eius falli essent. Postea doming expetenda in vel. Tota inani nec no. Nam id dicant veniam copiosae, solet aliquip recusabo vis ex.
+
+        </p>
+        <p class="highlighted" style="background-color: pink">
+            Lorem ipsum dolor sit amet, eos ne verterem disputationi, tale quaerendum nec et, est diam omittam at. Quando laoreet lobortis id his. Ubique noster temporibus in duo. Tantas ridens nam ei, sit quas exerci tacimates ne, sanctus recteque ei quo.
+
+Dico platonem in quo. Simul nusquam dolores ea sed. Te est eius falli essent. Postea doming expetenda in vel. Tota inani nec no. Nam id dicant veniam copiosae, solet aliquip recusabo vis ex.
+
+        </p>
+        <p id="first">     
+            Lorem ipsum dolor sit amet, eos ne verterem disputationi, tale quaerendum nec et, est diam omittam at. Quando laoreet lobortis id his. Ubique noster temporibus in duo. Tantas ridens nam ei, sit quas exerci tacimates ne, sanctus recteque ei quo.
+
+Dico platonem in quo. Simul nusquam dolores ea sed. Te est eius falli essent. Postea doming expetenda in vel. Tota inani nec no. Nam id dicant veniam copiosae, solet aliquip recusabo vis ex.
+
+        </p>
+    </body>
+</html>
