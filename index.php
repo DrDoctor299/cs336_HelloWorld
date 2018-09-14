2176a6a0550b8350a7350213187cb9474bba9933
DrDoctor299 <joshuwilliams@csumb.edu>
2018-09-14 11:48:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <li>             <a href="./lab/1/index.html">Lab 1</a>         </li>
-<li>             <a href="./homework/1/index.html> Homework 1</a> </li>
+<li>             <a href="./homework/1/index.html>Homework 1</a> </li>
