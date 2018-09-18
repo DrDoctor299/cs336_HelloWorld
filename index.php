db4f7bacd8987ae842f302850cdbd077eb5e46e8
Joshua Williams <joshuwilliams@csumb.edu>
2018-09-18 01:51:01 +0000

    fixing main index file

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <li>             <a href="./lab/1/index.html">Lab 1</a>         </li>
 <li>             <a href="./homework/1/index.html">Homework 1</a> </li>
+<li>             <a href="./lab/2/777/index.php">Lab 2</a>  </li>
