234203ecb1e8522eb51dea46382a1e8b89ab20ab
Joshua Williams <joshuwilliams@csumb.edu>
2018-10-11 06:51:59 +0000

    Updating Heroku Index

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <li>             <a href="./lab/1/index.html">Lab 1</a>         </li>
 <li>             <a href="./homework/1/index.html">Homework 1</a> </li>
 <li>             <a href="./lab/2/777/index.php">Lab 2</a>  </li>
+<li>             <a href="./lab/5/index.php">Lab 5</a>  </li>
