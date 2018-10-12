0142bca87bea6043461ee126c2702105bebb199a
Joshua Williams <joshuwilliams@csumb.edu>
2018-10-12 18:58:43 +0000

    Updating heroku index to include hw 3

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <li>             <a href="./homework/1/index.html">Homework 1</a> </li>
 <li>             <a href="./lab/2/777/index.php">Lab 2</a>  </li>
 <li>             <a href="./lab/5/index.php">Lab 5</a>  </li>
+<li>             <a href="./homework/3/index.php">Homework 3</a>  </li>
