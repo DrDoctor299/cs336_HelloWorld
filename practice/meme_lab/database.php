b63ad376d730d3139890a515c2c00ab5f1eeb8a3
Joshua Williams <joshuwilliams@csumb.edu>
2018-10-31 06:56:27 +0000

    Updating meme lab to run on heroku

--- practice/meme_lab/database.php
+++ practice/meme_lab/database.php
@@ -1,24 +1,53 @@
 <?php
 
+// // connect to our mysql database server
+
+// function getDatabaseConnection() {
+//     //===============Local DB============================
+//     // $host = "localhost";
+//     // $username = "joshuawilliams19";
+//     // $password = "cst336"; // best practice: define this in a separte file
+//     // $dbname = "meme_lab"; 
+    
+//     // // Create connection
+//     // $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+//     // $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+//     //==============Online DB=========================
+    
+//     $host = "us-cdbr-iron-east-01.cleardb.net";
+//     $username = "b49c6487e7ead7";
+//     $password = "c2f32a4c"; // best practice: define this in a separte file
+//     $dbname = "heroku_7e06c43769fa45b"; 
+    
+//     // Create connection
+//     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+//     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//     return $dbConn; 
+// }
+
+// // temporary test code
+// //$dbConn = getDatabaseConnection(); 
+//OLD
+//=============================================================================================
+//NEW
 // connect to our mysql database server
 
 function getDatabaseConnection() {
-    //===============Local DB============================
-    // $host = "localhost";
-    // $username = "joshuawilliams19";
-    // $password = "cst336"; // best practice: define this in a separte file
-    // $dbname = "meme_lab"; 
     
-    // // Create connection
-    // $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    // $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    //==============Online DB=========================
-    
-    $host = "us-cdbr-iron-east-01.cleardb.net";
-    $username = "b49c6487e7ead7";
-    $password = "c2f32a4c"; // best practice: define this in a separte file
-    $dbname = "heroku_7e06c43769fa45b"; 
+    if ($_SERVER['SERVER_NAME'] == "cs336-joshuawilliams1995.c9users.io") { // running on cloud9
+        $host = "localhost";
+        $username = "joshuawilliams19";
+        $password = "cst336"; // best practice: define this in a separte file
+        $dbname = "meme_lab"; 
+    } else {
+        // running on Heroku
+        $host = "us-cdbr-iron-east-01.cleardb.net";
+        $username = "b49c6487e7ead7";
+        $password = "c2f32a4c"; // best practice: define this in a separte file
+        $dbname = "heroku_7e06c43769fa45b"; 
+    }
     
     // Create connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -30,5 +59,8 @@ function getDatabaseConnection() {
 // temporary test code
 //$dbConn = getDatabaseConnection(); 
 
+echo "SERVER!!!!!!!!!!!!!!!!!! <br/>"; 
+echo $_SERVER['SERVER_NAME'];
+echo "<br>"; 
 
 ?>
