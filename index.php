28b502d8694ab3f52ee38fa35abec80504a15090
Joshua Williams <joshuwilliams@csumb.edu>
2018-11-10 21:12:07 +0000

    Adding files for lab 7, further testing on heroku to come

--- index.php
+++ index.php
@@ -4,4 +4,5 @@
 <li>             <a href="./lab/5/index.php">Lab 5</a>  </li>
 <li>             <a href="./homework/3/index.php">Homework 3</a>  </li>
 <li>             <a href="./lab/6/welcome.php">Lab 6</a>  </li>
+<li>             <a href="./lab/7/index.php">Lab 7</a>  </li>
 
