96eb6ec90e91c8706b1e28edd561c74dd8b9b286
Joshua Williams <joshuwilliams@csumb.edu>
2018-11-30 19:46:25 +0000

    Committing changes to password matching and styles

--- lab/9/index.php
+++ lab/9/index.php
@@ -1,19 +1,4 @@
-<?php
-$noMatch = false;
-if(isset($_POST)) {
-    if($_POST['password1'] != "" && $_POST['password2'] != ""){
-        if($_POST['password1'] != $_POST['password2']) {
-            global $noMatch;
-            $noMatch = true;
-        }
-        else {
-            header('Location: success.php');
-        }
-    }
-    
-}
-
-?>
+<!--TODO: Styling-->
 
 <!DOCTYPE html>
 <html>
@@ -21,56 +6,61 @@ if(isset($_POST)) {
         <title>
             Sign up
         </title>
-        
+        <link href="styles.css" rel="stylesheet" type="text/css" />
         <!--import jquery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>    
     </head>
     
     <body>
         <header>
-            Please enter your information below:
+            <h1>Please enter your information below:</h1> <br>
         </header>
         
         <form method="post" action="index.php">
             <!--Username-->
-            Enter Username: <br>
-            <input type="text" name="username" id="username"> <br>
-            <span style="color:red" id="usernameValidation"></span>  <br>
+            <div class="input">
+                Enter Username: <br>
+                <input type="text" name="username" id="username"> <br>
+                <span style="color:red" id="usernameValidation"></span>  <br>
+            </div>
 
             
             <!--password-->
-            Enter Password: <br>
-            <input type="password" name="password1"> <br>
-            Confirm password: <br>
-            <input type="password" name="password2"> <br>
-            <?php
-            global $noMatch;
-            if ($noMatch) {
-                echo "Please retype password <br>";
-            }
-            ?>
-            
+            <div class="input">
+                Enter Password: <br>
+                <input type="password" name="password1" id="password1"> <br>
+                Confirm password: <br>
+                <input type="password" name="password2" id="password2"> <br>
+                <span style="color:red" id="matchPasswords"></span>  <br>
+            </div>
             <!--zip-->
-            Enter ZIP: <br>
-            <input type="text" name="zip" id="zip"> <br>
-            <div id="zipbased"></div>
-            
+            <div class="input">
+                Enter ZIP: <br>
+                <input type="text" name="zip" id="zip"> <br>
+                <div id="zipbased"></div>
+            </div>
             <!--state-->
-            Enter State: <br>
-            <input type="text" name="state" id="state"> <br>
-            <div id="stateValid"></div>
+            <div class="input">
+                Enter State: <br>
+                <input type="text" name="state" id="state"> <br>
+                <div id="stateValid"></div>
+            </div>
             
             <!--county dropdown-->
-            <div id="county">
-                Select County: <br>
-                <select id="countySelect">
-                    <option value="invalidState">Select a state...</option>
-                </select>
+            <div class="input">
+                <div id="county">
+                    Select County: <br>
+                    <select id="countySelect">
+                        <option value="invalidState">Select a state...</option>
+                    </select>
+                </div>
             </div>
             
             <!--Submit-->
-            <br>
-            <input type="submit" value="Sign Up!">
+            <div class="input">
+                <br>
+                <input type="button" value="Sign Up!" id="submit">
+            </div>
         </form>
         <script>
             //update username with validation message
@@ -117,7 +107,7 @@ if(isset($_POST)) {
                     }
                 });
             });
-            //update
+            //update county dropdown on entering a state
             $("#state").change( function() {
                 $.ajax({
                     type: "get",
@@ -126,7 +116,7 @@ if(isset($_POST)) {
                     data: { "state": $(this).val()},
                     success: function(data,status) {
                         if(data == false) {
-                            $('#stateValid').html("Invalid state code (be sure to enter the two letter code)<br>");
+                            $('#stateValid').html("Invalid state code (be sure to enter a two letter code)<br>");
                         } 
                         else {
                             $('#stateValid').html("");
@@ -142,7 +132,64 @@ if(isset($_POST)) {
                     }
                 });
             });
-
+            //Submit button - check passwords match and submit if they do
+            $("#submit").click(function () {
+                if ($("#password1").val() == $("#password2").val()) {
+                    var data = {
+                        "username": $("#username").val(),
+                        "password": $("#password1").val(),
+                        "state": $("#state").val(),
+                        "zip": $("#zip").val(),
+                        "county": $("#countySelect option:selected").val()
+                    }   
+            
+                    console.log(JSON.stringify(data))
+                    
+                    $.ajax({
+                            // The URL for the request
+                            url: "success.php",
+        
+                            // Whether this is a POST or GET request
+                            type: "POST",
+        
+                            // The type of data we expect back
+                            // dataType: "json",
+        
+                            // contentType: "application/json",
+        
+                            data: JSON.stringify(data)
+        
+                        })
+                        // Code to run if the request succeeds (is done);
+                        // The response is passed to the function
+                        .done(function(data) {
+                            console.log(data);
+        
+                            // Do not do anything if there is no data
+                            if (!data || data.length == 0) return;
+                            
+                            window.location.href = "success.php"
+                        })
+                        // Code to run if the request fails; the raw request and
+                        // status codes are passed to the function
+                        .fail(function(xhr, status, errorThrown) {
+                            console.log("Sorry, there was a problem!");
+                            // console.log("Error: " + errorThrown);
+                            // console.log("Status: " + status);
+                            console.log(xhr.responseText);
+                            //console.dir(xhr);
+                        })
+                        // Code to run regardless of success or failure;
+                        .always(function(xhr, status) {
+                            console.log("The request is complete!");
+                        });
+                } 
+                else {
+                    //focus password1 and show red text warning passwords do not match
+                    $("#matchPasswords").html("Passwords do not match");
+                    $("#password1").focus();
+                }
+            });
         </script>
     </body>
 </html>
--- lab/9/success.php
+++ lab/9/success.php
@@ -1,13 +1,19 @@
-
+<?php
+// print_r($_POST);
+?>
 <html>
     <head>
         <title>
             Success!
         </title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
     </head>
     
     <body>
-        <h1>Successfully signed up!</h1>
+        <header>
+            <h1>Successfully signed up!</h1>
+        </header>
+        
         
         <a href="index.php">Sign up again?</a>
     </body>
